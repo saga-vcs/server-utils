e922ba09da68c6f68d58f3f79987dffe26334545
naterush <naterush1997@gmail.com>
2020-03-29 21:23:50 -0400

    add instructions to setup_public

--- setup_public.py
+++ setup_public.py
@@ -2,18 +2,17 @@
 This shell script automates the process of setting up a public saga server.
 It performs the following actions (conditionally on user prompts)
 
-
-
+1. Installs Node version manager and the correct version of node.
+2. Installs NGNIX, and sets it up for simple forwarding with to a domain
+   of the users choice.
+3. Installs lets-cert, adds https only connections to the server. 
 """
 
 
 
 
 def main():
-
-
-    pass
-
+    # TODO: figure out how to feed "yes" into a process!
 
 
 
